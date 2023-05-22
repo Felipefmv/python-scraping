e4d40570e14480d267945a323fbae3c1db115090
Felipe Fernandes <felipefernandesmiranda@gmail.com>
2023-05-22 16:36:01 -0300

    update to slenium 4

--- distribuição_sem_requisição.py
+++ distribuição_sem_requisição.py
@@ -1,77 +1,96 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.alert import Alert
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
 import pandas as pd
 
-email = input("Email: ")
-password = input("Senha: ")
-while True:
-    file = input("Caminho do arquivo: ")
+email = ""
+senha = ""
+df = pd.read_excel(
+    r"C:\Users\filipe.vieira\Downloads\.xlsx")
 
-    df = pd.read_excel(file)
-    driver = webdriver.Chrome(
-        executable_path=r"C:\webdrivers\chromedriver.exe")
-    print(df)
-    wait = WebDriverWait(driver, 20)
+options = Options()
+options.add_experimental_option("detach", True)
+options.add_argument('--allow-running-insecure-content')
+options.add_argument('--ignore-certificate-errors')
 
-    driver.get("https://scaweb.saude.gov.br/scaweb/")
-    driver.find_element_by_name("ds_email").send_keys(email)
-    driver.find_element_by_name("ds_senha_usuario").send_keys(password)
-    driver.find_element_by_name("acessar").send_keys(Keys.RETURN)
-    driver.find_element_by_id("sd3").send_keys(Keys.RETURN)
-    driver.find_element_by_id("proceed-button").send_keys(Keys.RETURN)
-    alerts = driver.switch_to.alert
-    alerts.accept()
-    menu = driver.find_element_by_id("j_id29:j_id48:menuDrop_13")
-    submenu = driver.find_element_by_id("j_id29:j_id48:menuItem_15")
-    ActionChains(driver).move_to_element(menu).click(submenu).perform()
 
+driver = webdriver.Chrome(options=options)
+driver.maximize_window()
+
+wait = WebDriverWait(driver, 20)
+
+driver.get("https://scaweb.saude.gov.br/scaweb/")
+search_elem = WebDriverWait(driver, 20).until(
+            EC.presence_of_element_located((By.NAME, "acessar")))
+
+driver.find_element(By.NAME,"ds_email").send_keys(email)
+driver.find_element(By.NAME,"ds_senha_usuario").send_keys(senha)
+driver.find_element(By.NAME,"acessar").send_keys(Keys.RETURN)
+driver.find_element(By.ID,"sd3").send_keys(Keys.RETURN)
+driver.find_element(By.ID,"proceed-button").click()
+driver.back()
+driver.find_element(By.ID,"sd3").send_keys(Keys.RETURN)
+driver.find_element(By.ID,"proceed-button").click()
+
+search_alert = WebDriverWait(driver, 20).until(
+            EC.alert_is_present())
+alerts = Alert(driver)
+alerts.accept()
+
+menu = driver.find_element(By.ID,"j_id29:j_id48:menuDrop_13")
+submenu = driver.find_element(By.ID,"j_id29:j_id48:menuItem_15")
+ActionChains(driver).move_to_element(menu).click(submenu).perform()
+
+wait.until(EC.element_to_be_clickable(
+    (By.ID, "requisicaoForm:itens:0:item_produto_dsProduto")))
+
+lastLine = len(df.index)
+lista = []
+for i in range(lastLine):
+    value = str(df.iloc[i, 4])
+    if pd.isna(df.iloc[i, 4]) or df.iloc[i, 4] == 0 or not value.isalnum() or value.isalpha():
+        lista.append(i)
+
+df.drop(index=lista, inplace=True)
+df2 = df.reset_index(drop=True)
+
+a = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100,
+     110, 120, 130, 140, 150, 160, 170, 180]
+b = 8
+c = 1
+print(df2)
+
+for j in df2.index:
+    code = df2.iat[j, 0]
+    amount = df2.iat[j, 4]
+    if j in a:
+        wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div["+str(
+            c)+"1]/div[3]/div[2]/form/div/div/div/table/tbody/tr/td["+str(b)+"]"))).click()
+
+        b += 1
+        c += 1
     wait.until(EC.element_to_be_clickable(
-        (By.ID, "requisicaoForm:itens:0:item_produto_dsProduto")))
+        (By.ID, "requisicaoForm:itens:"+str(j)+":item_produto_dsProduto"))).send_keys(code)
 
-    lastLine = len(df.index)
-    lista = []
-    for i in range(lastLine):
-        value = str(df.iloc[i, 4])
-        if pd.isna(df.iloc[i, 4]) or df.iloc[i, 4] == 0 or not value.isalnum() or value.isalpha():
-            lista.append(i)
+    driver.find_element(By.ID,
+        "requisicaoForm:itens:"+str(j)+":fireItem_produto_coSeqProdutoSuggest").send_keys(Keys.RETURN)
 
-    df.drop(index=lista, inplace=True)
-    df2 = df.reset_index(drop=True)
+    if (code == "BR0345099" or code == "BR0277319" or code == "BR0401411"):
+        wait.until(EC.element_to_be_clickable((By.ID, "requisicaoForm:itens:" +
+                   str(j)+":item_produto_coSeqProdutoSuggest:1:j_id306"))).click()
+    else:
+        wait.until(EC.element_to_be_clickable((By.ID, "requisicaoForm:itens:" +
+                   str(j)+":item_produto_coSeqProdutoSuggest:0:j_id306"))).click()
 
-    a = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100,
-         110, 120, 130, 140, 150, 160, 170, 180]
-    b = 8
-    c = 1
-    print(df2)
-
-    for j in df2.index:
-        code = df2.iat[j, 0]
-        amount = df2.iat[j, 4]
-        if j in a:
-            wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div["+str(
-                c)+"1]/div[3]/div[2]/form/div/div/div/table/tbody/tr/td["+str(b)+"]"))).click()
-
-            b += 1
-            c += 1
-        wait.until(EC.element_to_be_clickable(
-            (By.ID, "requisicaoForm:itens:"+str(j)+":item_produto_dsProduto"))).send_keys(code)
-
-        driver.find_element_by_id(
-            "requisicaoForm:itens:"+str(j)+":fireItem_produto_coSeqProdutoSuggest").send_keys(Keys.RETURN)
-
-        if (code == "BR0313379" or code == "BR0401411"):
-            wait.until(EC.element_to_be_clickable((By.ID, "requisicaoForm:itens:" +
-                                                   str(j)+":item_produto_coSeqProdutoSuggest:1:j_id306"))).click()
-        else:
-            wait.until(EC.element_to_be_clickable((By.ID, "requisicaoForm:itens:" +
-                                                   str(j)+":item_produto_coSeqProdutoSuggest:0:j_id306"))).click()
-
-        driver.find_element_by_id(
-            "requisicaoForm:itens:"+str(j)+":item_qtSolicitado").send_keys(amount)
-        driver.find_element_by_id("requisicaoForm:addItem").click()
+    driver.find_element(By.ID,
+        "requisicaoForm:itens:"+str(j)+":item_qtSolicitado").send_keys(amount)
+    driver.find_element(By.ID,"requisicaoForm:addItem").click()
 
 exit()
